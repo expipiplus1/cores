3be2aa1a6ea32691b8b9cdd50a33f9045014f2d0
Joe Hermaszewski <spaceman@monoid.al>
2015-05-19 00:13:30 +0100

    Add required header to usb_serial.h

--- teensy3/usb_serial.h
+++ teensy3/usb_serial.h
@@ -66,6 +66,7 @@ extern volatile uint8_t usb_configuration;
 // C++ interface
 #ifdef __cplusplus
 #include "Stream.h"
+#include "core_pins.h" // for millis()
 class usb_serial_class : public Stream
 {
 public:
